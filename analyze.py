bde0f0b7e6768dda45b86e87a2ab1f2d4f5046de
Cam Askew (Work) <caskew@snapchat.com>
2017-09-17 19:55:52 -0700

    Make matplotlib work on ubuntu server

--- analyze.py
+++ analyze.py
@@ -1,5 +1,9 @@
 from essentia.standard import *
+import matplotlib
+# Force matplotlib to not use any Xwindows backend.
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+
 filename = 'input/wav/city-of-stars'
 wavFilename = filename + '.wav'
 pngFilename = filename + '.png'
